64dc88e47529d77320c6cbceb1580295997678b7
zrq-github <46354863+zrq-github@users.noreply.github.com>
2022-07-22 17:36:42 +0800

    获取当前进程（程序）主窗体句柄并设置wpf的父窗体为此句柄

---
+++ ZRQ.UI.Utils/ProcessHelper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZRQ.UI.Utils
+{
+    internal static class ProcessHelper
+    {
+        private static class Win32
+        {
+            internal const uint GwOwner = 4;
+
+            internal delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+
+            [DllImport("User32.dll", CharSet = CharSet.Auto)]
+            internal static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
+
+            [DllImport("User32.dll", CharSet = CharSet.Auto)]
+            internal static extern int GetWindowThreadProcessId(IntPtr hWnd, out IntPtr lpdwProcessId);
+
+            [DllImport("User32.dll", CharSet = CharSet.Auto)]
+            internal static extern IntPtr GetWindow(IntPtr hWnd, uint uCmd);
+
+            [DllImport("User32.dll", CharSet = CharSet.Auto)]
+            internal static extern bool IsWindowVisible(IntPtr hWnd);
+        }
+
+        public static IntPtr GetProcessHandle(int processId)
+        {
+            IntPtr processPtr = IntPtr.Zero;
+
+            Win32.EnumWindows((hWnd, lParam) =>
+            {
+                IntPtr pid;
+                Win32.GetWindowThreadProcessId(hWnd, out pid);
+
+                if (pid == lParam &&
+                    Win32.IsWindowVisible(hWnd) &&
+                    Win32.GetWindow(hWnd, Win32.GwOwner) == IntPtr.Zero)
+                {
+                    processPtr = hWnd;
+                    return false;
+                }
+
+                return true;
+
+            }, new IntPtr(processId));
+
+            return processPtr;
+        }
+    }
+}
---
+++ ZRQ.UI.Utils/Test/获取鼠标点击的对象.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace ZRQ.UI.Utils.Test
+{
+    internal class 获取鼠标点击的对象
+    {
+        private void MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            DataGrid dataGrid = sender as DataGrid;
+            if (dataGrid == null) return;
+
+            System.Windows.Point aP = e.GetPosition(dataGrid);
+            IInputElement obj = dataGrid.InputHitTest(aP);
+            DependencyObject target = obj as DependencyObject;
+
+            while (target != null)
+            {
+                if (target is DataGrid) break;
+
+                if (target is CheckBox) break;
+
+                if (target is DataGridRow)
+                {
+                    break;
+                }
+                target = VisualTreeHelper.GetParent(target);
+            }
+        }
+    }
+}

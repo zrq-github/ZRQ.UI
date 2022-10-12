def294acfdc27eff04c0afc007160816ce9c52f0
zrq-github <46354863+zrq-github@users.noreply.github.com>
2022-10-12 17:55:23 +0800

    天机 relaycommand

---
+++ ZRQ.UI.Shared/UIUtils/RelayCommand.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ZRQ.UIShared.UIUtils
+{
+    class RelayCommand : ICommand
+    {
+        Func<object, bool> _canExecute;
+        Action<object> _execute;
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+        public RelayCommand(Action<object> execute)
+    : this(execute, null)
+        {
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                if (_canExecute != null)
+                {
+                    CommandManager.RequerySuggested += value;
+                }
+            }
+            remove
+            {
+                if (_canExecute != null)
+                {
+                    CommandManager.RequerySuggested -= value;
+                }
+            }
+        }
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecute == null) return true;
+            return _canExecute(parameter);
+        }
+        public void Execute(object parameter)
+        {
+            if (_execute != null && CanExecute(parameter))
+            {
+                _execute(parameter);
+            }
+        }
+    }
+}

c3795f5010a37e675a4a4d8411478a6e13453031
zrq-github <46354863+zrq-github@users.noreply.github.com>
2022-11-17 11:39:48 +0800

    增加一些提示

---
+++ Test/ZRQ.UIBehaviors/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ZRQ.UIBehaviors
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public string? Text { get; set; } = "Text";
+
+        /// <summary>
+        /// 由事件触发和事件一致参数的方法
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+        }
+    }
+}
+1
---
+++ Test/ZRQ.UIBehaviors/reademe.md
@@ -0,0 +1,3 @@
+﻿# readme
+
+主要是测试 Behaviors 的一些使用方法

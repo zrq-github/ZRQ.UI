083374b34f8a90631e422d09a182090ad35e5288
zrq-github <46354863+zrq-github@users.noreply.github.com>
2022-12-24 15:48:31 +0800

    更新一些课反转操作的判断

--- ZRQ.UI.Share/UIConverter/BoolToVisibilityConverter.cs
+++ ZRQ.UI.Share/UIConverter/BoolToVisibilityConverter.cs
@@ -4,11 +4,23 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Data;
 using System.Windows.Media;
 
 namespace ZRQ.UI.UIConverter
 {
+    /// <summary>
+    /// 转换枚举
+    /// </summary>
+    public enum ConvertEnum
+    {
+        #region bool
+        TrueToVisible,
+        TrueToHidden,
+        #endregion
+    }
+
     /// <summary>
     /// bool -> Visibility
     /// </summary>
@@ -16,48 +28,76 @@ namespace ZRQ.UI.UIConverter
     /// false -> Visibility.Hidden
     /// true -> Visibility.Visible
     /// </remarks>
-    public class BoolToVisibilityConverter : System.Windows.Data.IValueConverter
+    public class BoolToVisibilityConverter : IValueConverter
     {
         public static readonly BoolToVisibilityConverter Instance = new();
 
         object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value is bool)
+            if (value is not bool convertValue) return false;
+
+            if (parameter is ConvertEnum convertEnum)
             {
-                bool isShow = (bool)value;
-                if (isShow)
-                {
-                    return System.Windows.Visibility.Visible;
-                }
-                else
-                {
-                    return System.Windows.Visibility.Hidden;
-                }
+                return Convert(convertValue, convertEnum);
             }
-            else
+
+            // 不反转
+            return Convert(convertValue, ConvertEnum.TrueToVisible);
+        }
+
+        private static object Convert(bool convertValue, ConvertEnum convertEnum)
+        {
+            if (convertValue)
             {
-                return System.Windows.Visibility.Hidden;
+                if (convertEnum == ConvertEnum.TrueToVisible) return Visibility.Visible;
+                if (convertEnum == ConvertEnum.TrueToHidden) return Visibility.Hidden;
             }
+
+            if (!convertValue)
+            {
+                if (convertEnum == ConvertEnum.TrueToVisible) return Visibility.Hidden;
+                if (convertEnum == ConvertEnum.TrueToHidden) return Visibility.Visible;
+            }
+
+            throw new NotImplementedException();
+        }
+
+        private static object ConvertBack(Visibility visibility, ConvertEnum convertEnum = ConvertEnum.TrueToVisible)
+        {
+            if (visibility == Visibility.Visible)
+            {
+                if (convertEnum == ConvertEnum.TrueToVisible)
+                    return Visibility.Visible;
+
+                if (convertEnum == ConvertEnum.TrueToHidden)
+                    return Visibility.Hidden;
+            }
+
+            if(visibility == Visibility.Hidden)
+            {
+                if (convertEnum == ConvertEnum.TrueToVisible)
+                    return Visibility.Hidden;
+
+                if (convertEnum == ConvertEnum.TrueToHidden)
+                    return Visibility.Visible;
+            }
+
+            throw new NotImplementedException();
         }
 
         object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value is System.Windows.Visibility)
+            if (value is not Visibility visibility)
             {
-                System.Windows.Visibility visibility = (System.Windows.Visibility)value;
-                if (visibility == System.Windows.Visibility.Visible)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
+                throw new NotImplementedException();
             }
-            else
+
+            if(parameter is ConvertEnum convertEnum)
             {
-                return false;
+                return ConvertBack(visibility, convertEnum);
             }
+
+            return ConvertBack(visibility);
         }
     }
 }

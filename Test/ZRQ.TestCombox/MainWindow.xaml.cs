eccb1401da5ec496cb724e05ba8ad252eeb66def
zrq-github <46354863+zrq-github@users.noreply.github.com>
2023-01-11 10:17:25 +0800

    TestCombox

---
+++ Test/ZRQ.TestCombox/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ZRQ.UI.UIConverter;
+
+namespace ZRQ.TestCombox
+{
+    [TypeConverter(typeof(EnumDescriptionTypeConverter))]
+    public enum DisplayStyle
+    {
+        [Description("没有")]
+        None,
+
+        [Description("一")]
+        One,
+
+        [Description("二")]
+        Tow,
+    }
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Test/ZRQ.TestCombox/MainWindowsModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZRQ.UI.UIModel;
+
+namespace ZRQ.TestCombox
+{
+    internal class MainWindowsModel : ViewModelBase
+    {
+
+        private System.Collections.IEnumerable comboBoxItems;
+
+        public System.Collections.IEnumerable ComboBoxItems { get => comboBoxItems; set => SetProperty(ref comboBoxItems, value); }
+
+        private object displayStyle;
+
+        public object DisplayStyle { get => displayStyle; set => SetProperty(ref displayStyle, value); }
+
+    }
+}
